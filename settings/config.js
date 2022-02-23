633d1c08a0863011b7b4ef9579632220159ac84e
AzenMKS <kandang1127@gmail.com>
2022-02-23 10:42:13 +0700

    SUBSCRIBE MAUL BOTZ

---
+++ settings/config.js
@@ -0,0 +1,26 @@
+const fs = require('fs')
+const chalk = require('chalk')
+
+// self or public
+global.self = false //jadiin true klo gk mau fitur bot lu di pke sama org lain
+
+// setting
+global.ownername ="MAUL BOTZ"
+global.ownernumber = "62895416489526"
+global.botname = "𝙽𝚊𝚋𝚒𝚕𝙱𝚘𝚝"
+global.thumbnail = fs.readFileSync("./settings/maul.jpg") //sesuaikan dengan nama foto
+global.background = "https://telegra.ph/file/d4c05638fa7886a1d8060.jpg"
+global.limit = {
+		free:20,
+		premium:1000
+	}
+global.session_name = "maul.json"
+// jangan ubah nama session entar erorr
+
+let file = require.resolve(__filename)
+fs.watchFile(file, () => {
+	fs.unwatchFile(file)
+	console.log(chalk.redBright(`Update'${__filename}'`))
+	delete require.cache[file]
+	require(file)
+})
